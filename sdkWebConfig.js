acad73d7bf9f8856ef486532b7722345940f880c
Dinakaran <110745337+Dina-petrick@users.noreply.github.com>
2025-12-17 09:44:51 +0000

    sdk added

--- sdkWebConfig.js
+++ sdkWebConfig.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	let script = document.createElement("script");
 	script.setAttribute("defer", "defer");
 	script.setAttribute("fcm_service_path", "firebase-messaging-sw.js?v=1.2");
-	script.src =  "https://sdk.resu.team/handlers/2f533ec9fd6348ae874877a03f155409.sdk";
+	script.src =  "https://sdk.resul.team/handlers/2f533ec9fd6348ae874877a03f155409.sdk";
 	// script.setAttribute("resulconfig","resulconfig.json");
 	document.head.appendChild(script);
   });
